8ef3ce9214fcc2d99f0c2629d4f1143f1a5cfed5
ismail antar <antartalha@gmail.com>
2025-11-08 15:35:31 -0500

    mandatory id done

--- p3/script.sh
+++ p3/script.sh
@@ -55,9 +55,20 @@ sudo chmod +x /usr/local/bin/argocd
 # Expose Argo CD UI
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
 
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 -d)
+
+echo "🔑 Logging into Argo CD..."
+argocd login localhost:8080 \
+  --username admin \
+  --password "$ARGOCD_PASSWORD" \
+  --insecure
 
 argocd app create myapp \
   --repo https://github.com/iantar1/Inception-Of-Things.git \
   --path p3 \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace dev
+
+
+argocd app sync myapp
