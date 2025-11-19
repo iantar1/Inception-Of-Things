e9c2370a6657d44e6fe20c1b92837fc35478c8ad
ismail antar <antartalha@gmail.com>
2025-11-19 12:21:22 -0500

    to save

---
+++ bonus/working.sh
@@ -0,0 +1,13 @@
+sudo helm upgrade --install gitlab gitlab/gitlab \
+  --namespace gitlab --create-namespace \
+  --timeout 600s \
+  --set global.hosts.domain=localhost \
+  --set global.hosts.externalIP=127.0.0.1 \
+  --set certmanager-issuer.email="you@example.com" \
+  --set global.gitaly.enabled=true \
+  --set global.gitaly.replicaCount=1 \
+  --set global.minio.enabled=true \
+  --set global.postgresql.install=true \
+  --set global.redis.install=true \
+  --set global.registry.enabled=false \
+  --set global.prometheus.install=false
